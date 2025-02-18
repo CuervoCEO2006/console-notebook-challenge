317b550cb8c4e4916de50f17abef004aa5685802
Simon Muñoz Montoya <simoncho0907@gmail.com>
2025-02-18 13:17:09 -0500

    Añadí los métodos add tag y __str__

--- app/notebook.py
+++ app/notebook.py
@@ -1,6 +1,24 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
+from datetime import datetime
+
+
 class Note:
+    HIGH: str = 'HIGH'
+    MEDIUM: str = 'MEDIUM'
+    LOW: str = 'LOW'
+
     def __init__(self, code: str, title: str, text: str, importance: str):
-        HIGH: str = 'HIGH'
-        MEDIUM: str = 'MEDIUM'
-        LOW: str = 'LOW'
+        self.code = code
+        self.title = title
+        self.text = text
+        self.importance = importance
+        self.creation_date = datetime.now()
+        self.tags = []
+
+    def add_tag(self, tag):
+        if tag not in self.tags:
+            self.tags.append(tag)
+
+    def __str__(self):
+        return f"Date: {self.creation_date}\n{self.title}: {self.text}"
+

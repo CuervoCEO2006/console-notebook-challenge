c475586b7fc5348676a56cd8b8109b8ff4d5a15e
Simon Muñoz Montoya <simoncho0907@gmail.com>
2025-02-18 12:30:37 -0500

    Creé la clase Note, incluí las constantes y los atribuos que deben seer inicalizados en el contructor.

--- app/notebook.py
+++ app/notebook.py
@@ -1,1 +1,6 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
+class Note:
+    def __init__(self, code: str, title: str, text: str, importance: str):
+        HIGH: str = 'HIGH'
+        MEDIUM: str = 'MEDIUM'
+        LOW: str = 'LOW'

fa97e741781006ab74baf6e2568d2ab191418977
Juan José Cuervo <juanjosecu234@gmail.com>
2026-02-16 18:55:46 -0500

    Clase Note creada con parametros definidos

--- src/console.py
+++ src/console.py
@@ -1,1 +1,14 @@
 # TODO: Agrega el código de las clases de la interfaz de usuario aquí. Borra este comentario al terminar.
+
+
+
+
+class Note:
+    HIGH: str = "HIGH"
+    MEDIUM: str = "MEDIUM"
+    LOW: str = "LOW"
+
+
+
+
+
